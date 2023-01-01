a623755b72aff70e6fd313ae91a701b057ff86f7
1Kworld <93234344+1Kworld@users.noreply.github.com>
2022-12-31 20:46:57 -0800

    Add files via upload

---
+++ src/pages/lal/LookaLike.js
@@ -0,0 +1,147 @@
+import './LookaLike.css.css';
+
+export default function LookaLike(){
+    return(
+        <body>
+            <header class="bg-dark py-5">
+                <div class="container px-4 px-lg-6 my-6">
+                    <div class="text-center text-white">
+                        <h1 class="display-4 fw-bolder">LOOKALIKES</h1>
+                        <p class="lead fw-normal text-white-50 mb-0">1K Original Prints</p>
+                    </div>
+                </div>
+            </header>
+            {/* <!--- NAV--> */}
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-bottom">
+                <div class="container px-4 px-lg-6">
+                    <a class="navbar-brand" href="/include/globe/theglobe.html">1K Globe</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                        aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarResponsive">
+                        <ul class="navbar-nav ml-auto">
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        
+            {/* <!-- Section--> */}
+            <section class="py-5">
+                <div class="container px-4 px-lg-0 mt-0">
+                    <div class="row gx-4 gx-lg-1 row-cols-2 row-cols-md-3 row-cols-xl-6 justify-content-center">
+                        <div class="col mb-5">
+                            <div class="card h-100">
+                                {/* <!-- Product image--> */}
+                                <img class="card-img-top" src="/assets/LAL_00.png"   alt="..." />
+                                {/* <!-- Product details--> */}
+                                <div class="card-body p-4">
+                                    <div class="text-center">
+                                        {/* <!-- Product name--> */}
+                                        <h5 class="fw-bolder">UA_00</h5>
+                                        {/* <!-- Product price--> */}
+                                    </div>
+                                </div>
+                                {/* <!-- Product actions--> */}
+                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="/include/LAL/post/UA_00.html" target="_blank">View</a></div>
+                                </div>
+                            </div>
+                        </div>
+        
+                        <div class="col mb-5">
+                            <div class="card h-100">
+                                {/* <!-- Product image--> */}
+                                <img class="card-img-top" src="/assets/LAL/UA_2.png" alt="..." />
+                                {/* <!-- Product details--> */}
+                                <div class="card-body p-4">
+                                    <div class="text-center">
+                                        {/* <!-- Product name--> */}
+                                        <h5 class="fw-bolder">UA_01</h5>
+                                        {/* <!-- Product price--> */}
+                                    
+                                    </div>
+                                </div>
+                                {/* <!-- Product actions--> */}
+                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="/include/LAL/post/UA_01.html" target="_blank">View</a></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col mb-5">
+                            <div class="card h-100">
+                                {/* <!-- Product image--> */}
+                                <img class="card-img-top" src="/assets/LAL/UA_3.png" alt="..." />
+                                {/* <!-- Product details--> */}
+                                <div class="card-body p-4">
+                                    <div class="text-center">
+                                        {/* <!-- Product name--> */}
+                                        <h5 class="fw-bolder">UA_02</h5>
+                                        {/* <!-- Product price--> */}
+                                    </div>
+                                </div> 
+                                {/* <!-- Product actions--> */}
+                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="/include/LAL/post/UA_02.html" target="_blank">View</a></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col mb-5">
+                            <div class="card h-100">
+                                {/* <!-- Product image--> */}
+                                <img class="card-img-top" src="/assets/LAL/UA_01.png"   alt="..." />
+                                {/* <!-- Product details--> */}
+                                <div class="card-body p-4">
+                                    <div class="text-center">
+                                        {/* <!-- Product name--> */}
+                                        <h5 class="fw-bolder">UA_03</h5>
+                                        {/* <!-- Product price--> */}
+                                    </div>
+                                </div>
+                                {/* <!-- Product actions--> */}
+                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="/include/LAL/post/UA_03.html" target="_blank">View</a></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col mb-5">
+                            <div class="card h-100">
+                                {/* <!-- Product image--> */}
+                                <img class="card-img-top" src="/assets/LAL/UA_02.png"   alt="..." />
+                                {/* <!-- Product details--> */}
+                                <div class="card-body p-4">
+                                    <div class="text-center">
+                                        {/* <!-- Product name--> */}
+                                        <h5 class="fw-bolder">UA_04</h5>
+                                        {/* <!-- Product price--> */}
+                                    </div>
+                                </div>
+                                {/* <!-- Product actions--> */}
+                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="/include/LAL/post/UA_04.html" target="_blank">View</a></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col mb-5">
+                            <div class="card h-100">
+                                {/* <!-- Product image--> */}
+                                <img class="card-img-top" src="/assets/LAL/UA_03.png"   alt="..." />
+                                {/* <!-- Product details--> */}
+                                <div class="card-body p-4">
+                                    <div class="text-center">
+                                        {/* <!-- Product name--> */}
+                                        <h5 class="fw-bolder">UA_05</h5>
+                                        {/* <!-- Product price--> */}
+                                    </div>
+                                </div>
+                                {/* <!-- Product actions--> */}
+                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="/include/LAL/post/UA_05.html" target="_blank">View</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </body>
+    );
+}
